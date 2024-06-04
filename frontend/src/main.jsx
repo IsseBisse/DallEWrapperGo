92cea19a3559d83af21ba817fe47e411ca883109
Isak Strömberg <isak.stromberg@gmail.com>
2024-06-04 21:03:17 +0200

    Removed extra root file

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-// This is probably redundant with index.js
-// TODO: Test and if so, remove this
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
